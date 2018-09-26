a85f33620a4fe9df34c38805062e99208a776a39
Mayanthi Perera <mayanthi@netwealth.com.au>
2018-09-26 15:18:16 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name=args[0];
+            Console.WriteLine("Hello "+name);
+        }
+    }
+}
